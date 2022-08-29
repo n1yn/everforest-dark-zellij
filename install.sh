d4fa37fd4226dc4727aa801d62d7452a73ba9a35
Fabian D <62945074+ghostcrafter551@users.noreply.github.com>
2022-08-29 20:02:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.config/zellij/themes && curl -sS 'https://raw.githubusercontent.com/ghostcrafter551/everforest-dark-zellij/main/everforest.yaml' > ~/.config/zellij/themes/everforest.yaml && grep 'theme:*' ~/.config/zellij/config.yaml && sed -i '/theme:/c\theme: everforest-dark-medium' ~/.config/zellij/config.yaml || echo "theme: everforest-dark-medium" >> ~/.config/zellij/config.yaml
+mkdir -p ~/.config/zellij/themes && curl -sS 'https://raw.githubusercontent.com/ghostcrafter551/everforest-dark-zellij/main/everforest.yaml' > ~/.config/zellij/themes/everforest.yaml && touch ~/.config/zellij/config.yaml && grep 'theme:*' ~/.config/zellij/config.yaml && sed -i '/theme:/c\theme: everforest-dark-medium' ~/.config/zellij/config.yaml || echo "theme: everforest-dark-medium" >> ~/.config/zellij/config.yaml
